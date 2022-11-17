e37004ab5bc4f589cc1e35c519dac5d17ef65eb2
Alex Castelo <alexcastelo437@gmail.com>
2022-11-17 20:38:23 -0300

    customer and address model created

---
+++ src/main/java/one/digitalinnovation/DIO/model/EnderecoRepository.java
@@ -0,0 +1,8 @@
+package one.digitalinnovation.DIO.model;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends CrudRepository<Endereco, String> {
+}
